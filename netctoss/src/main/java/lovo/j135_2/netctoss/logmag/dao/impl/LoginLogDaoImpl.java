d7830ecc7beb406fd60ea3a199ee967c18972cdb
LAPTOP-JDGQJIVK\sc <suchang1999@126.com>
2017-05-10 17:24:07 +0800

    1.2

--- netctoss/src/main/java/lovo/j135_2/netctoss/logmag/dao/impl/LoginLogDaoImpl.java
+++ netctoss/src/main/java/lovo/j135_2/netctoss/logmag/dao/impl/LoginLogDaoImpl.java
@@ -1,11 +1,15 @@
 package lovo.j135_2.netctoss.logmag.dao.impl;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Repository;
 
+import lovo.j135_2.netctoss.logmag.beans.LoginLogBean;
+import lovo.j135_2.netctoss.logmag.beans.LoginLogPageBean;
+import lovo.j135_2.netctoss.logmag.beans.OperateLogBean;
 import lovo.j135_2.netctoss.logmag.dao.ILoginLogDao;
 import lovo.j135_2.netctoss.logmag.mapper.LoginLogMapper;
 
@@ -23,14 +27,34 @@ public class LoginLogDaoImpl implements ILoginLogDao {
 	}
 
 	@Override
-	public Long findLoginLogIdByLoginDate(Timestamp loginTime) {
+	public List<LoginLogBean> findLoginLogBeanByTimeToPage(LoginLogPageBean pageBean,String beginTime, String endTime) throws Exception {
 		// TODO Auto-generated method stub
-		return loginLogMapper.findLoginLogIdByLoginDate(loginTime);
+		return loginLogMapper.findLoginLogBeanByTimeToPage(pageBean,beginTime, endTime);
 	}
 
 	@Override
-	public void updateLoginLog(Timestamp quitTime, Long id) {
+	public int findCountLoginLogBeanByTimeToPage(String beginTime, String endTime) {
 		// TODO Auto-generated method stub
-		loginLogMapper.updateLoginLog(quitTime, id);
+		return loginLogMapper.findCountLoginLogBeanByTimeToPage(beginTime, endTime);
 	}
+
+	@Override
+	public List<LoginLogBean> findAllLoginLogBeanByTimeToPage(String beginTime, String endTime) {
+		// TODO Auto-generated method stub
+		return loginLogMapper.findAllLoginLogBeanByTimeToPage(beginTime, endTime);
+	}
+
+	@Override
+	public LoginLogBean findLoginLogBeanById(Long id) {
+		// TODO Auto-generated method stub
+		return loginLogMapper.findLoginLogBeanById(id);
+	}
+
+	@Override
+	public List<OperateLogBean> findOperateLogById(Long id) {
+		// TODO Auto-generated method stub
+		return loginLogMapper.findOperateLogById(id);
+	}
+
+	
 }
